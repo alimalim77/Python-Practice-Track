3990b0754bbed16d468e60306e636222393e208b
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-01-19 17:14:29 +0530

    Practice Marathon

--- Educative Track/buy_sell_stock.py
+++ Educative Track/buy_sell_stock.py
@@ -1,16 +1,12 @@
 #buy sell stock
-li = [310,315,275,295,260,270,290,230,255,250]
+prices = [310,315,275,295,260,270,290,230,255,250]
 
-
-tupmax = tuple((li[0],0))
-tupmin = tuple((li[0],0))
-ctr= li[0]
 profit_daily = []
 
-for i in range(0,len(li)):
-    for j in range(i+1,len(li)-1):
-        if li[j+1] - li[i+1] > li[j]-li[i]:
-            profit = li[j+1] - li[i+1]
+for i in range(0,len(prices)):
+    for j in range(i+1,len(prices)-1):
+        if prices[j+1] - prices[i+1] > prices[j]-prices[i]:
+            profit = prices[j+1] - prices[i+1]
             profit_daily.append(profit)
 
 
