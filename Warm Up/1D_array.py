6b46710969ffa4920e2ac61bf820d8f4631741c8
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-01-20 17:33:29 +0530

    Practice Marathon

--- Warm Up/1D_array.py
+++ Warm Up/1D_array.py
@@ -1,35 +1,9 @@
 #implementing 1D array
-class array:
-    def __init__(self,size):
-        self.final_size=size
-        self.length=0
-        self.data=[]
-        
-    def add(self,element):
-        if self.final_size > self.length:
-            self.data.append(element)
-            self.length+=1
-            print("added",element)
-            
-        else:
-            print("srray is full")
-            
-    def deleteat(self,index):
-        for i in range(index ,self.length-1):
-            self.data[i]=self.data[i+1]
-        del self.data[self.length-1]
-        self.length-=1
-        
-        
-    def insertat(self,index,item):
-        if self.final_size > self.length:
-            for i in range(self.length,index, -1):
-                self.data[i]=self.data[i-1]
-            self.data[index]=item
-            self.length +=1
-        else:
-            print("array is full")
+inp = int(input("Enter the number to list"))
+li = []
 
-array_obj=array(5)
-for i in range (10):
-    array_obj.add("apple")
+for i in range(inp):
+    get = int(input("Enter the number"))
+    li.append(get)
+
+print(li) 
