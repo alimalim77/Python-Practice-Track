0db43776a2de952892a45c157f53e4f842f54975
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-01-20 16:50:43 +0530

    Practice Marathon

--- Educative Track/arbitaryprecision.py
+++ Educative Track/arbitaryprecision.py
@@ -1,25 +1,25 @@
 #arbitaryprecision
 
-def foo(naam):
-    total = -(len(naam))
+def foo(element):
+    total = -(len(element))
     i=-1
 
     while i >= total:
         if i == -1:
-            naam[i] = naam[i]+1
+            element[i] = element[i]+1
 
-        if naam[i] >= 10:
-            sub = naam[i]%10
-            naam[i] = sub
-            naam[i-1] = naam[i-1] + 1
+        if element[i] >= 10:
+            sub = element[i]%10
+            element[i] = sub
+            element[i-1] = element[i-1] + 1
         
-        if naam[0] == 10:
-            naam[0]=1
-            naam.append(0)
+        if element[0] == 10:
+            element[0]=1
+            element.append(0)
         i -= 1
     
-    print(naam)
-    retlist = "".join(map(str,naam))
+    print(element)
+    retlist = "".join(map(str,element))
     print(retlist)
 
 
