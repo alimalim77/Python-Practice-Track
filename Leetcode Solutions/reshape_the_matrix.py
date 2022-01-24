9bddce0dd2883885bab561aa00ca87d47241c995
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-01-24 14:36:54 +0530

    Practice Marathon

---
+++ Leetcode Solutions/reshape_the_matrix.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def matrixReshape(self, mat, r, c):
+        """
+        :type mat: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        
+        a = 0 
+        for i in mat:
+            a  = a + len(i)
+
+        if a != r * c:
+            return mat
+            
+        ans = [[]]
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                k = mat[i][j]
+                if len(ans[-1]) < c:
+                    ans[-1].append(k)
+                else:
+                    newm = []
+                    newm.append(k)
+                    ans.append(newm)
+        return ans
