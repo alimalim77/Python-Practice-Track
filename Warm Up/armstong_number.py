1ea8fa23a225c63eccedb9d9f978d7b44e47c47c
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-01-28 06:03:20 +0530

    Practice Marathon

--- Warm Up/armstong_number.py
+++ Warm Up/armstong_number.py
@@ -1,13 +1,23 @@
 #check armstrong number
-inp = int(input("Enter a number"))
-first = inp
-sum=0
-rem=0
+def armstrongfunction(inp):
+    first = inp
+    sum=0
+    rem=0
 
-while inp!= 0:
-    rem = inp % 10
-    inp = inp//10
-    sum = sum + rem**3
+    while inp!= 0:
+        rem = inp % 10
+        inp = inp//10
+        sum = sum + rem**3
 
-if sum == first:
-    print("Armstrong Number")
+    if sum == first:
+        result = str(sum) + "is Armstrong Number"
+        return result
+    else:
+        return None
+
+
+start, end = map(int,input("Enter the values").split())
+for i in range(start,end+1):
+    res = armstrongfunction(i)
+    if res != None:
+        print(res, ' is Armstrong Number', end ="\n")
