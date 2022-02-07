df4e5a6a5031e99564527265c219fab9eec5323c
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-02-08 00:08:23 +0530

    Practice Marathon

--- Educative Track/string_to_integer.py
+++ Educative Track/string_to_integer.py
@@ -6,17 +6,15 @@ def str_to_int(mystr):
         is_neg = False
     
     inp = 0
-    if mystr == '0':
-        return 0
-    else:
-        i = 0
-        while i < len(mystr):
-            inp = inp * 10 + (int(mystr[i]))
-            i += 1
+    
+    i = 0
+    while i < len(mystr):
+        inp = inp * 10 + (ord(mystr[i]) - ord('0'))
+        i += 1
     if is_neg:
         return inp * -1
     else:
         return inp
 
 
-print(str_to_int('-1090'))
+print(str_to_int('-121'))
