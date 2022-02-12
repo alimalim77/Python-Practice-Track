cb1ea86d6ca8a7a320d9fe4c00f7b0a3641fdaa9
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-02-12 18:34:28 +0530

    Practice Marathon

--- Educative Track/circular_linked_list.py
+++ Educative Track/circular_linked_list.py
@@ -1,4 +1,7 @@
 
+from multiprocessing import Value
+
+
 class Node:
     def __init__(self,value):
         self.value = value
@@ -29,6 +32,9 @@ class CircularLinkedList:
         node.next = self.head
 
     def printlist(self):
+        if self.head == None:
+            print('No Linked List')
+            return
         cur = self.head
         print(cur.value)
         cur = cur.next
@@ -37,6 +43,28 @@ class CircularLinkedList:
                 break
             print(cur.value)
             cur = cur.next
+    
+    def remove(self,val):
+        cur = self.head
+        if self.head.next == self.head:
+            if val == self.head.value:
+                self.head = None
+            else:
+                print('Not exists')
+        prev = cur
+        cur = cur.next
+        while cur != self.head:
+            if cur.value == val:
+                prev.next = cur.next
+                cur = cur.next
+            else:
+                prev = cur
+                cur = cur.next
+        if self.head.value == val and self.head.next != self.head:
+            self.head = self.head.next
+            cur.next = self.head
+
+        
         
 ll = CircularLinkedList()
 ll.append(30)
@@ -47,4 +75,6 @@ ll.append(87)
 ll.append(92)
 
 ll.printlist()
-        
+ll.remove(87)
+print('--')
+ll.printlist()
