b0e70e87de8c06ad33f81b9a0bfda6bd68732f66
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-02-26 17:05:55 +0530

    Practice Marathon

--- Leetcode Solutions/first_and_last_occurence,py.py
+++ Leetcode Solutions/first_and_last_occurence,py.py
@@ -1,15 +1,20 @@
-def searchRange(nums , target):
+def searchRange(nums, target):
+        if nums == [] or target not in nums:
+            print([-1,-1])
         ini = 0
-        mid = len(nums)//2
         tot = len(nums)
+        mid = (ini+tot)//2
+        
         pos1 = 0
         pos2 = 0
         while ini <= tot:
             if nums[mid] == target:
-                if mid == 0 or nums[mid-1] != target:
+                lower = mid -1
+                if mid == 0 or nums[lower] != target:
                     pos1 = mid
                     break
                 tot = mid-1
+                mid = (ini+tot)//2
                     
             if target < nums[mid]:
                 tot = mid -1
@@ -20,22 +25,23 @@ def searchRange(nums , target):
             
         
         ini = 0
-        mid = len(nums)//2
         tot = len(nums)
+        mid = (ini+tot)//2
         while ini <= tot:
             if nums[mid] == target:
-                if mid == len(nums)-1 or nums[mid+1] != target:
+                upper = mid+1
+                if mid == len(nums)-1 or nums[upper] != target:
                     pos2 = mid
                     break
                 ini = mid + 1
                 mid = (ini+tot)//2
+            
             if target < nums[mid]:
                 tot = mid -1
                 mid = (ini+tot)//2
             elif target > nums[mid]:
                 ini = mid+1
                 mid = (ini+tot)//2
-            
-        print(pos1,pos2)
+        print([pos1,pos2])
 
 searchRange( [5,7,7,8,8,10], 8)
