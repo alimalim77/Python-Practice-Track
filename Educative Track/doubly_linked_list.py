146eefaf823f04f6d3a7a93e0626dd571fc9f036
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-02-27 14:46:20 +0530

    Practice Marathon

--- Educative Track/doubly_linked_list.py
+++ Educative Track/doubly_linked_list.py
@@ -30,6 +30,33 @@ class DoublyLinkedList:
         pre.next = cur
         self.head = pre
     
+    def add_after(self,key,val):
+        bon = Node(val)
+        if self.head == None:
+            return
+        cur = self.head
+        while cur and cur.val != key:
+            cur = cur.next
+        bon.next = cur.next
+        cur.next.prev = bon
+        bon.prev = cur
+        cur.next = bon
+    
+    def add_before(self,key,val):
+        bon = Node(val)
+        if self.head == None:
+            return
+        cur = self.head
+        while cur and cur.val != key:
+            cur = cur.next
+        temp = cur.prev
+        bon.next = cur
+        cur.prev = bon
+        temp.next = bon
+        bon.prev = temp
+
+            
+    
     def printlist(self):
         if not self.head:
             return
@@ -37,13 +64,16 @@ class DoublyLinkedList:
         while cur:
             print(cur.val)
             cur = cur.next 
-
+#prepend and add before wrok differently, add before will not work for head and will 
+# require further changes
 dd = DoublyLinkedList()
 dd.append(5)
 dd.append(7)
 dd.append(15)
 dd.append(74)
+dd.add_after(7,69)
 dd.prepend(2)
+dd.add_before(15,900)
 dd.append(9)
 dd.printlist()
 
