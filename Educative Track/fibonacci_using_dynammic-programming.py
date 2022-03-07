a1148635a763a345175a6ea3df07712cda02e379
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-03-07 16:58:45 +0530

    Practice Marathon

---
+++ Educative Track/fibonacci_using_dynammic-programming.py
@@ -0,0 +1,15 @@
+
+con ={}
+def fib(n):
+    print(con)
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n in con:
+        return con[n]
+    else:
+        con[n] =  fib(n-1) + fib(n-2)
+        return con[n]
+
+print(fib(7))
