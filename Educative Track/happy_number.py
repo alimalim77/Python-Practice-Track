156b1df33161fa3fab3943eaa7761c82173bd468
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-03-10 00:26:41 +0530

    Practice Marathon

--- Educative Track/happy_number.py
+++ Educative Track/happy_number.py
@@ -1,15 +1,14 @@
 s = set()
 def isHappy(n):
+    if n == 1:
+        return True
     li = list(map(int,str(n)))
     sqli = [x*x for x in li]
     b = sum(sqli)
     if b in s:
         return False
-    if b == 1:
-        return True
-    
     else:
         s.add(b)
         return isHappy(b)
 
-print(isHappy(2))
+print(isHappy(7))
