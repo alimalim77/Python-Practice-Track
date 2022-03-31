06e61899af7851bc26b0cd2718f47ece62f27291
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-03-31 09:55:02 +0530

    Practice Marathon

--- Educative Track/smallest subarray with sum.py
+++ Educative Track/smallest subarray with sum.py
@@ -12,6 +12,7 @@ print(smallestsub([1,2,3,4,5],15))
 #18/19 Cases Passed : TLE Error
 
 #Approach No. 2
+"""
 def minSubArrayLen(nums, target):
         win_start, win_end = 0,0
         win_sum = 0
@@ -31,25 +32,25 @@ def minSubArrayLen(nums, target):
             return 0
         return min_len
     
-
-print(minSubArrayLen([2,1,5,2,3,2],7))
-
+"""
 #Approach 3
 # Is hould have used 
-'''
-      win_start, win_end = 0,0
-        win_sum = 0
-        min_len = float('inf')
+def minSubArrayLen(nums, target):
+    win_start, win_end = 0,0
+    win_sum = 0
+    min_len = float('inf')
         
-        while win_end < len(nums):
-            win_sum += nums[win_end]
-            while win_sum >= target:
-                min_len = min(min_len,win_end - win_start+1)
-                win_sum -= nums[win_start]
-                win_start += 1
+    while win_end < len(nums):
+        win_sum += nums[win_end]
+        while win_sum >= target:
+            min_len = min(min_len,win_end - win_start+1)
+            win_sum -= nums[win_start]
+            win_start += 1
             
-            win_end += 1
-        if min_len == float('inf'):
-            return 0
-        return min_len
-'''
+        win_end += 1
+    if min_len == float('inf'):
+        return 0
+    return min_len
+print(minSubArrayLen([2,1,5,2,3,2],7)) #2
+print(minSubArrayLen([2,1,5,2,8],7)) #1 
+print(minSubArrayLen([3,4,1,1,6],8)) #3
