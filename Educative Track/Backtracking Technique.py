c19217412a369d0c7243ede21f5c48e07f6c140d
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-08-04 17:12:03 +0530

    Problems Coding

--- Educative Track/Backtracking Technique.py
+++ Educative Track/Backtracking Technique.py
@@ -1,13 +1,17 @@
 from pyparsing import Word
 
 
-def bct(n,li = []): 
+def bct(n,li): 
     print(li)
-    if len(li) >= 3:
+    if len(li) >= n:
         return
-    for i in Word:
-        li.append(i)
+    if li and li[-1] == 'X':
+        flag = False
+    else:
+        flag = True
+    for i in range(n):
+        li.append(li[0]) if flag else li.append(li[1])
         bct(n,li)
         li.pop()
 
-print(bct())
+print(bct(5,['X','Y']))
