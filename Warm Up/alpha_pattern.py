abd6c1cdede2ed546a5aa0181740f04d60a43bf3
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-08-29 15:59:46 +0530

    Pattern Programs

--- Warm Up/alpha_pattern.py
+++ Warm Up/alpha_pattern.py
@@ -1,11 +1,17 @@
-# A = 65
-# BB = 66
-# CCC
-# DDDD
-con = 1
-inp = int(input())
-for i in range(65,65+inp,1):
-    for j in range(i,i+con,1):
-        print(chr(i),end="")
-    con += 1
-    print("")
+'''
+Alpha Pattern
+A = 1
+BB = 2
+CCC = 3
+'''
+
+def pattern(n):
+    count  = 1
+
+    for i in range(65,65+n,1):
+        for j in range(i,i + count,1):
+            print(chr(i),end="")
+        count += 1
+        print()
+    
+pattern(int(input("Enter the number")))
--- Warm Up/character_reversal.py
+++ Warm Up/character_reversal.py
@@ -1,20 +1,18 @@
 '''
-Character Pattern
-A
-BC
-DEF
-GHIJ
+Character Reversal
+D
+CD
+BCD
+ABCD
 '''
 
 def pattern(n):
-    a = 65
+    a = (ord('A')+n-1)
 
     for i in range(1,n+1,1):
         for j in range(0,i,1):
-            print(chr(a+j),end="")
-        a += (j+1)
+            print(chr(a+j),end= "")
+        a = (ord('A')+n-1)-i
         print()
-    
-    
 
 pattern(int(input("Enter the number")))
