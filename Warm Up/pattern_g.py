bc88f7060e9a9db87c1050da26dfef37cb4fe4e7
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-09-21 23:46:18 +0530

    PrepBytes and GFG Ques

--- Warm Up/pattern_g.py
+++ Warm Up/pattern_g.py
@@ -1,13 +1,33 @@
-#Not Considering stars
+# def fun():
+#     n = 6
+#     for i in range(n):
+#         for j in range(n):
+#             #or ((i==6//2) and ((1)<j<3))
+#             if (((0<i<n-1)  and (j==0))) or (((i==0 or i ==n-1) and (0<j<(n-1)))) or ((j==n-1) and (((n//2)<=i<n-1)) or (i==(n//2)) and ((n//2)<=j<n-1)):
+#                 print("*",end="")
+#             else:
+#                 print(" ",end="")
+#         print()
+            
+
+# fun()
+
+# #Not Considering stars
+from collections import defaultdict
 def fun():
-    for i in range(6):
-        for j in range(6):
+    g = defaultdict(list)
+    g[1].append(3)
+    print(g)
+    n = 6
+    for i in range(n):
+        for j in range(n):
             #or ((i==6//2) and ((1)<j<3))
-            if (((0<i<5)  and (j==0))) or (((i==0 or i ==5) and (0<j<4))) or ((j==5) and (((6//2)<=i<5)) or (i==(6//2)) and ((6//2)<=j<5)):
-                print("* ",end="")
+            if ( (0<i<n-1)  and (j==0) ) or ((i==0 or i ==n-1) and (0<j<=n//2)) or ( (j==n//2+1) and  (n//2<=i<n-1) )  or(i==n//2) and (n//2<=j<=n-1) :
+                print("*",end="")
             else:
                 print(" ",end="")
         print()
             
 
 fun()
+
