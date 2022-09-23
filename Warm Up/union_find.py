4c82808540b08870d43b41ee4db0e9e9dd8d98c4
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-09-23 23:35:20 +0530

    Changes to union find

--- Warm Up/union_find.py
+++ Warm Up/union_find.py
@@ -3,7 +3,7 @@ class UnionFind:
         self.data = [i for i in range(data)]
     
     def union(self,x,y):
-        if self.connection(self,x,y):
+        if self.connection(x,y):
             return True
         conA = self.find(x)
         conB = self.find(y)
@@ -19,4 +19,18 @@ class UnionFind:
     def connection(self,x,y):
         return self.find(x) == self.find(y)
 
-    
+
+uf = UnionFind(10)
+# 1-2-5-6-7 3-8-9 4
+uf.union(1, 2)
+uf.union(2, 5)
+uf.union(5, 6)
+uf.union(6, 7)
+uf.union(3, 8)
+uf.union(8, 9)
+print(uf.connected(1, 5))  # true
+print(uf.connected(5, 7))  # true
+print(uf.connected(4, 9))  # false
+# 1-2-5-6-7 3-8-9-4
+uf.union(9, 4)
+print(uf.connected(4, 9))  # true
