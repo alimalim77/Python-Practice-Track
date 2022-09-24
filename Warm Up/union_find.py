e1d22b483dae288ca66b0de37fc925931f4adc6f
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-09-24 16:01:20 +0530

    Changes to union way

--- Warm Up/union_find.py
+++ Warm Up/union_find.py
@@ -1,3 +1,4 @@
+#Union Find - Quick Union
 class UnionFind:
     def __init__(self,data) -> None:
         self.data = [i for i in range(data)]
@@ -7,14 +8,14 @@ class UnionFind:
             return True
         conA = self.find(x)
         conB = self.find(y)
-
-        while conB != self.data[conB]:
-            conB = self.data[conB]
-    
-        self.data[conB] = conA
+        
+        if conA != conB:
+            self.data[conB] = conA
 
     def find(self,x):
-        return self.data[x]
+        while x != self.data[x]:
+            x = self.data[x]
+        return x
     
     def connection(self,x,y):
         return self.find(x) == self.find(y)
@@ -28,9 +29,9 @@ uf.union(5, 6)
 uf.union(6, 7)
 uf.union(3, 8)
 uf.union(8, 9)
-print(uf.connected(1, 5))  # true
-print(uf.connected(5, 7))  # true
-print(uf.connected(4, 9))  # false
+print(uf.connection(1, 5))  # true
+print(uf.connection(5, 7))  # true
+print(uf.connection(4, 9))  # false
 # 1-2-5-6-7 3-8-9-4
 uf.union(9, 4)
-print(uf.connected(4, 9))  # true
+print(uf.connection(4, 9))  # true
