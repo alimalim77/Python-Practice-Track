45d7b24d32c4672ed2eb990f4c8acf7cf004bff5
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-09-25 23:03:11 +0530

    grokking path

--- Educative Track/maximum_sum_subarray_bruteforce.py
+++ Educative Track/maximum_sum_subarray_bruteforce.py
@@ -1,9 +1,23 @@
 def maxArray(num, k):
-    maxsum = [0]
-    for i in range(0,len(num)-k+1,1):
-        if sum(num[i:i+k]) > sum(maxsum):
-            maxsum = num[i:i+k]
-    return maxsum
+    # maxsum = [0]
+    # for i in range(0,len(num)-k+1,1): #O(n)
+    #     if sum(num[i:i+k]) > sum(maxsum): #O(n)
+    #         maxsum = num[i:i+k] 
+    # return maxsum
+    win_start,win_end = 0,0
+    totsum = 0
+    maxNum = float("-inf")
+
+    for i in range(0,len(num)):
+        totsum += num[win_end]
+        maxNum = max(maxNum,totsum)
+        win_end += 1
+
+        if win_end - win_start == k:
+            totsum -= num[win_start]
+            win_start += 1 
+    print(maxNum)
+     
 
 print(maxArray([2,3,4,1,5],2)) #[3,4]
 print(maxArray([2,1,5,1,3,2],3)) #[5,1,3]
