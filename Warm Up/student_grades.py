47d4e34d4f40920bc253b22a9aa1706b1b87e3f0
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-09-25 21:15:06 +0530

    gfg work

---
+++ Warm Up/student_grades.py
@@ -0,0 +1,15 @@
+grade = 89
+if grade >= 90:
+    print("A+")
+elif grade >= 80:
+    print("A")
+elif grade >= 70:
+    print("B")
+elif grade >= 60:
+    print("C")
+elif grade >= 50:
+    print("D")
+elif grade >= 40:
+    print("E")
+else:
+    print("F")
