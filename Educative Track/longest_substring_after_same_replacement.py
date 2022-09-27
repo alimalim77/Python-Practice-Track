a8de250bbc5b21ce152669469db6cba61916cb4f
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-09-27 22:20:12 +0530

    Grokking Path Revision

--- Educative Track/longest_substring_after_same_replacement.py
+++ Educative Track/longest_substring_after_same_replacement.py
@@ -1,24 +1,65 @@
 #aabccbb and 2 gives output 5 because bbbbb
 
-def inputfunction(mystr,num):
-    win_start, maxLen, maxRepeat = 0,0,0
-    freqMap = {}
+# def inputfunction(mystr,num):
+#     win_start, maxLen, maxRepeat = 0,0,0
+#     freqMap = {}
 
-    totalLen = len(mystr)
-    for win_end in range(totalLen):
-        rightChar = mystr[win_end]
-        if rightChar not in freqMap:
-            freqMap[rightChar] = 0
-        freqMap[rightChar] += 1
-        maxRepeat = max(maxRepeat,freqMap[rightChar])
+#     totalLen = len(mystr)
+#     for win_end in range(totalLen):
+#         rightChar = mystr[win_end]
+#         if rightChar not in freqMap:
+#             freqMap[rightChar] = 0
+#         freqMap[rightChar] += 1
+#         maxRepeat = max(maxRepeat,freqMap[rightChar])
     
-        if (win_end-win_start+1-maxRepeat) > num:
-            leftChar = mystr[win_start]
-            freqMap[leftChar] -= 1
+#         if (win_end-win_start+1-maxRepeat) > num:
+#             leftChar = mystr[win_start]
+#             freqMap[leftChar] -= 1
+#             win_start += 1
+    
+#         maxLen = max(maxLen,win_end-win_start+1)
+#     return maxLen
+
+
+def inputfunction(mystr,k):
+    win_start,win_end = 0,0
+    maxlen = float("-inf")
+    hashmap = {}
+
+    for _ in range(len(mystr)):
+        cur = mystr[win_end]
+        if cur not in hashmap:
+            hashmap[cur] = 0
+        hashmap[cur] += 1
+        n = k
+
+        
+        duphash = {cur:1} 
+        temp = win_start
+        while (n>0 and win_start < win_end):
+            pos = mystr[win_start]
+
+            if pos != cur:
+                duphash[cur] += 1
+                n -= 1
+            else:
+                duphash[cur] += 1
             win_start += 1
-    
-        maxLen = max(maxLen,win_end-win_start+1)
-    return maxLen
+        
+        if win_start < win_end-1:
+            win_start = temp + 1 # 7
+        else:
+            win_start = temp
+        
+        
+        if len(duphash) == 1:
+            maxlen = max(maxlen,win_end-win_start+1)
+        win_end += 1
+    return maxlen
+             
+
+        
+
    
 print(inputfunction("aabccbb",2))
 print(inputfunction("abbcb",1))
--- Educative Track/no_repeat_substring.py
+++ Educative Track/no_repeat_substring.py
@@ -5,25 +5,47 @@ Explanation: The longest substring without any repeating characters is "abc".
 '''
 
 
-from collections import Counter
-def norepeat(mystr):
-    se = set()
-    win_start = 0
-    win_end = 0
-    maxlen = float("-inf")
+# from collections import Counter
+# def norepeat(mystr):
+#     se = set()
+#     win_start = 0
+#     win_end = 0
+#     maxlen = float("-inf")
     
-    while win_end < len(mystr):
+#     while win_end < len(mystr):
         
-        if mystr[win_end] not in se:
-            se.add(mystr[win_end])
-            win_end += 1
+#         if mystr[win_end] not in se:
+#             se.add(mystr[win_end])
+#             win_end += 1
         
+#         else:
+#             win_start += 1
+#             win_end = win_start
+#             maxlen = max(len(se),maxlen)
+#             se.clear()
+#     return maxlen
+
+#from collections import Counter
+def norepeat(mystr):
+    win_start, win_end = 0,0
+    maxlen = float("-inf")
+    s = {}
+
+    for _ in range(len(mystr)):
+        cur = mystr[win_end]
+        if cur not in s:
+            s[cur] = 1
         else:
-            win_start += 1
-            win_end = win_start
-            maxlen = max(len(se),maxlen)
-            se.clear()
+            while s[cur] > 0:
+                pos = mystr[win_start]
+                s[pos] -= 1
+                win_start += 1
+            s[cur] = 1
+        maxlen = max(maxlen,win_end-win_start+1)
+        win_end += 1
     return maxlen
+
+
 
 
 print(norepeat("aabccbb")) #3
