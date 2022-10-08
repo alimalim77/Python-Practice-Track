ddf739826c53c5b294e92ab2603f149113790550
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-10-08 17:18:15 +0530

    GFG

---
+++ Warm Up/star_between_identical.py
@@ -0,0 +1,9 @@
+def recfun(index,string,final):
+    if index == final:
+        return string
+    if 0 < index < final and string[index-1] == string[index]:
+         return recfun(index+1, string[:index] + "*" + string[index:], final+1)
+    return recfun(index +1, string, final)
+
+mystr = "abccbbaff"
+print(recfun(0,mystr,len(mystr)))
