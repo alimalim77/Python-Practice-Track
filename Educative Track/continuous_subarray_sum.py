3a3d333047e157e0e2e9edf8630d59977606b83d
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-11-02 04:06:19 +0530

    Changes to graph

--- Educative Track/continuous_subarray_sum.py
+++ Educative Track/continuous_subarray_sum.py
@@ -1,32 +1,29 @@
-# def fun(creators,ids,views):
-#     creatorHashmap = {}
-#     for i,j in zip(creators,views):
-#         if i not in creatorHashmap:
-#             creatorHashmap[i] = 0
-#         creatorHashmap[i] += j
-#     largest = max(creatorHashmap.values())
-#     creatorList = {}
-#     for name,view in creatorHashmap.items():
-#         if view == largest:
-#             creatorList[name] = 0
+def fun(creators,ids,views):
+    creatorHashmap = {}
+    for i,j in zip(creators,views):
+        if i not in creatorHashmap:
+            creatorHashmap[i] = 0
+        creatorHashmap[i] += j
+    largest = max(creatorHashmap.values())
+    creatorList = {}
+    for name,view in creatorHashmap.items():
+        if view == largest:
+            creatorList[name] = 0
         
-#     viewmap = {}
-#     for i,j,k in zip(creators,views,ids):
-#         if i in creatorList:
-#                 #creatorList[i] = max(j,creatorList[i])
-#             if j > creatorList[i]:
-#                 creatorList[i] = j
-#                 viewmap[i] = k
+    viewmap = {}
+    for i,j,k in zip(creators,views,ids):
+        if i in creatorList:
+                #creatorList[i] = max(j,creatorList[i])
+            if j > creatorList[i]:
+                creatorList[i] = j
+                viewmap[i] = k
         
                     
                 
-#     res = []
-#     for i,j in viewmap.items():
-#         res.append([i,j])
-#     return res
+    res = []
+    for i,j in viewmap.items():
+        res.append([i,j])
+    return res
 
 
-# print(fun(["a"],["a"],[0]))
-char = 'A'
-
-print(ord(char)-ord('A'))
+print(fun(["a"],["a"],[0]))
